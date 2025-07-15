9159209b3d1447cc4fd59018bd73e674f56c19c9
Sevinda-Herath <sevindaherath@gmail.com>
2025-07-15 13:18:29 +0000

    API Works

--- predict.js
+++ predict.js
@@ -1,5 +1,5 @@
 // Predict Section JavaScript
-const API_BASE_URL = 'http://0.0.0.0:8000';
+const API_BASE_URL = 'http://162.243.175.58:8000';
 
 // Initialize the predict section
 document.addEventListener('DOMContentLoaded', function() {
